d6311d07fca060d14ae89fc119c5211de3f01b2d
ksakacep <konrad.socha10@wp.pl>
2020-01-20 00:39:35 +0100

    Final commit - added comment

--- klient.py
+++ klient.py
@@ -5,14 +5,14 @@ from time import sleep
 import socket
 import emoji
 
+# Polaczenie sie z serwerem poprzez gniazdo
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect(('192.168.0.25', 1237))
+sock.connect(('192.168.0.26', 1237))
 
-# GUI:
+# GUI
 app = QApplication([])
-gap = QLabel()
 
-# Chat window:
+# Okno czatu
 text_area = QPlainTextEdit()
 text_area.setFocusPolicy(Qt.NoFocus)
 message = QLineEdit()
@@ -26,7 +26,7 @@ window.setWindowTitle("Czat")
 window.setLayout(layout)
 
 
-# Login window:
+# Okno logowania
 label = QLabel()
 label.setText("Podaj swoj numer:")
 login_number = QLineEdit()
@@ -42,12 +42,13 @@ login_window.setWindowTitle("Logowanie")
 login_window.setLayout(login_layout)
 
 
-# Main window:
+# Menu glowne
 logged_label = QLabel()
 logged_label.setAlignment(Qt.AlignRight)
 chatters_label = QLabel()
 chatters_label.setText("Aktywne rozmowy:")
 chatters_list = QListWidget()
+gap = QLabel()
 new_label = QLabel()
 new_label.setText("Podaj numer adresata:")
 new_chatter = QLineEdit()
@@ -67,7 +68,7 @@ main_window.setWindowTitle("SocketGG")
 main_window.setLayout(main_layout)
 
 
-# Event handlers:
+# Inicjalizacja zmiennych globalnych
 new_messages = {}
 chat_history = {}
 chatters = []
@@ -79,6 +80,7 @@ login = ""
 mutex = Lock()
 
 
+# Uzupelnianie listy aktywnych konwersacji
 def fill_up_list():
     global chatters, new_messages, chatters_list
 
@@ -91,14 +93,15 @@ def fill_up_list():
         chatters_list.addItem(item)
 
 
+# Obsluga listy aktywnych konwersacji
 def refresh_list():
     global chatters, new_messages, chatters_list
-
+    # Dodawanie nowych pozycji na liscie aktywnych rozmow
     if len(chatters) != chatters_list.count():
         fill_up_list()
+    # Zmiana statusow rozmow (dzwoneczek)
     else:
-        i = 0
-        for one in chatters:
+        for i, one in enumerate(chatters):
             if new_messages[one]:
                 x = one + " " + emoji.emojize(':bell:')
                 y = chatters_list.item(i).text()
@@ -109,9 +112,9 @@ def refresh_list():
                 y = chatters_list.item(i).text()
                 if x != y:
                     chatters_list.item(i).setText(x)
-            i += 1
 
 
+# Obsluga wiadomosci odebranych od serwera
 def fetch_new_messages():
     global new_messages, chatters, sock
 
@@ -120,12 +123,14 @@ def fetch_new_messages():
         response = response.decode("utf-8")
         if ';' in response:
             [sender, new_message] = response.split(';', 1)
+            # Dodaj rozmowce jako element slownika new_messages
             if sender not in new_messages.keys():
                 mutex.acquire()
                 try:
                     new_messages[sender] = []
                 finally:
                     mutex.release()
+            # Dodaj nowego rozmowce, jesli do niego nie pisalismy
             if sender not in chatters and sender != "$":
                 chatters.append(sender)
                 chat_history[sender] = []
@@ -137,20 +142,24 @@ def fetch_new_messages():
         sleep(.5)
 
 
+# Inicjalizacja watku odbierajacego nowe wiadomosci od serwera
 thread = Thread(target=fetch_new_messages, daemon=True)
 thread.start()
 
 
+# Wyswietlanie nowych wiadomosci w oknie czatu
 def display_new_messages():
     global chatter, window, new_messages, text_area, chat_history
 
     while window.isVisible() and (new_messages[chatter] or new_messages["$"]):
         mutex.acquire()
+        # Wiadomosci systemowe
         if new_messages["$"]:
             try:
                 text_area.appendPlainText("Error: " + new_messages["$"].pop(0))
             finally:
                 mutex.release()
+        # Wiadomosci uzytkownikow
         else:
             try:
                 temp = chatter + ": " + new_messages[chatter].pop(0)
@@ -160,6 +169,7 @@ def display_new_messages():
             chat_history[chatter].append(temp)
 
 
+# Wyswietlanie historii czatu
 def display_chat_history():
     global chat_history, text_area
 
@@ -167,6 +177,7 @@ def display_chat_history():
         text_area.appendPlainText(msg)
 
 
+# Wyswietlanie okna czatu
 def open_chat(number):
     global chatter, window, text_area, message, new_chatter
 
@@ -180,6 +191,7 @@ def open_chat(number):
     display_chat_history()
 
 
+# Otwarcie czatu z nowym rozmowca poprzez wpisanie numeru
 def open_chat_new():
     global new_chatter, new_messages, chatters, chat_history
 
@@ -195,6 +207,7 @@ def open_chat_new():
         print("Numer powinien skladac sie tylko z cyfr.")
 
 
+# Otwarcie czatu z nowym rozmowca poprzez liste aktywnych rozmow
 def open_chat_list(item):
     if emoji.emojize(':bell:') in item.text():
         [new, _] = item.text().split(' ')
@@ -204,6 +217,7 @@ def open_chat_list(item):
     open_chat(new)
 
 
+# Obsluga nowej wiadomosci wychodzacej
 def send_message():
     global chatter, login, sock, chat_history, message
 
@@ -216,6 +230,7 @@ def send_message():
         text_area.appendPlainText("Error: Wiadomosc jest za dluga.")
 
 
+# Logowanie
 def log_in():
     global login, sock, logged_label, login_window, main_window
 
@@ -229,24 +244,27 @@ def log_in():
         print("Numer powinien skladac z maksymalnie 16 cyfr.")
 
 
+# Wyswietlenie okna logowania
 login_window.show()
+
+# Laczenie elementow interfejsu z ich funkcjami
 login_button.clicked.connect(log_in)
 login_number.returnPressed.connect(log_in)
-
 new_button.clicked.connect(open_chat_new)
 new_chatter.returnPressed.connect(open_chat_new)
-
 chatters_list.itemClicked.connect(open_chat_list)
-
 message.returnPressed.connect(send_message)
 
+# Wlaczenie automatycznego uruchamiania funkcji co okreslony interwal
 timer = QTimer()
 timer.timeout.connect(display_new_messages)
 timer.start(300)
-
 list_timer = QTimer()
 list_timer.timeout.connect(refresh_list)
 list_timer.start(200)
 
+# Uruchomienie aplikacji
 app.exec_()
+
+# Wylogowanie uzytkownika
 sock.send(bytes('0', "utf-8"))
--- server.c
+++ server.c
@@ -13,6 +13,7 @@
 #define buf_size 128
 #define max_nr_len 16
 
+// Struktura opisujaca pojedyncza wiadomosc
 struct Message {
     char *senderNumber;
     char *text;
@@ -20,6 +21,7 @@ struct Message {
 
 typedef struct Message Message;
 
+// Struktura opisujaca pojedynczego uzytkownika
 struct User {
     char *number;
     Message *messages;
@@ -31,30 +33,35 @@ struct User {
 
 typedef struct User User;
 
+// Struktura opisujaca deskryptor
 struct cln {
     int cfd;
     struct sockaddr_in caddr;
 };
 
+// Inicjalizacja zmiennych globalnych
 User *users;
 int userCount = 0;
-
+// Utworzenie semafora kontrolujacego wspolbiezny dostep do tablicy users
 pthread_mutex_t users_mutex = PTHREAD_MUTEX_INITIALIZER;
 
+// Przeslanie wiadomosci do uzytkownika, gdy ten jest zalogowany
 void passMessages(int id) {
     while(users[id].messageCount > 0 && users[id].loggedIn == 1)
     {
         pthread_mutex_lock(&users[id].mutex);
 
         char buf[buf_size] = "";
+        // Konkatenacja numeru adresata, separatora oraz tresci wiadomosci
         strcat(buf, users[id].messages[0].senderNumber);
         strcat(buf, ";");
         strcat(buf, users[id].messages[0].text);
 
         write(users[id].cfd, &buf, buf_size);
-        printf("Przekazalem wiadomosc: %s\n", buf);
+        printf("Przekazano wiadomosc: %s\n", buf);
 
         users[id].messageCount--;
+        // Zdjecie przeslanej wiadomosci ze stosu oczekujacych wiadomosci
         if(users[id].messageCount > 0)
             memmove(&users[id].messages[0], &users[id].messages[1], sizeof(Message)*users[id].messageCount);
         users[id].messages = realloc(users[id].messages, sizeof(Message)*users[id].messageCount);
@@ -63,6 +70,7 @@ void passMessages(int id) {
     }
 }
 
+// Zwroc id (indeks na liscie 'users') uzytkownika o danym numerze
 int findUser(char *number)
 {
     for(int i=0; i<userCount; i++){
@@ -72,32 +80,37 @@ int findUser(char *number)
     return -1;
 }
 
+// Obsluga klienta
 void *serve_single_client(void *arg) {
     int bytesRead;
     char buf[buf_size];
-    char flag;
-    char *found; //wskaznik na znaleziony srednik w stringu
-    int index; //index znalezionego srednika
-    int id = -2; //id obslugiwanego klienta (na liscie 'users')
-    char recvNumber[max_nr_len]; //numer odbiorcy nadanej przez obslugiwanego uzytkownika wiadomosci
-    int recvId; //id odbiorcy wiadomosci (na liscie 'users')
+    char flag; // rodzaj odebranej od klienta wiadomosci
+    char *found; // wskaznik na znaleziony srednik w stringu
+    int index; // index znalezionego srednika
+    int id = -2; // id obslugiwanego klienta (indeks na liscie 'users')
+    char recvNumber[max_nr_len]; // numer odbiorcy nadanej przez obslugiwanego uzytkownika wiadomosci
+    int recvId; // id odbiorcy wiadomosci (indeks na liscie 'users')
     struct cln* c = (struct cln*)arg;
-    printf("\nConnection from %s:%d\n", inet_ntoa(c->caddr.sin_addr), c->caddr.sin_port);
+    printf("\nNowe polaczenie od %s:%d\n", inet_ntoa(c->caddr.sin_addr), c->caddr.sin_port);
 
     while(1) {
+        // Czyszczenie zmiennych 'buf' i 'recvNumber'
         memset(buf, 0, buf_size);
         memset(recvNumber, 0, max_nr_len);
+        // Odebranie od klienta wiadomosci (w sposob nieblokujacy)
         bytesRead = recv(c->cfd, buf, buf_size, MSG_DONTWAIT);
         if(bytesRead > 0) {
             flag = buf[0];
             printf("\nOdebrano wiadomosc: %s\n", buf);
+            // Skrocenie zawartosci bufora o pierwszy znak (flage)
             memmove(&buf[0], &buf[1], buf_size);
             switch(flag) {
+                // Logowanie/rejestracja uzytkownika
                 case '1':
                     if(id == -2) {
                         id = findUser(buf);
-                        if(id == -1) {
-                            //inicjalizacja nowego uzytkownika
+                        if(id == -1) { // uzytkownik nie znajduje sie na liscie users
+                            // Inicjalizacja nowego uzytkownika
                             User user;
                             user.number = malloc(sizeof(buf));
                             strcpy(user.number, buf);
@@ -105,6 +118,7 @@ void *serve_single_client(void *arg) {
                             user.messages = malloc(sizeof(struct Message));
                             user.messageCount = 0;
                             user.loggedIn = 1;
+                            // Inicjalizacja semafora kontrolujacego dostep do danych uzytkownika
                             user.mutex = (pthread_mutex_t)PTHREAD_MUTEX_INITIALIZER;
 
                             pthread_mutex_lock(&users_mutex);
@@ -116,7 +130,7 @@ void *serve_single_client(void *arg) {
 
                             printf("> Nowy uzytkownik o numerze: %s\n", buf);
                         }
-                        else {
+                        else { // logowanie istniejacego uzytkownika
                             users[id].loggedIn = 1;
                             users[id].cfd = c->cfd;
                             printf("> Zalogowano uzytkownika: %s\n", buf);
@@ -126,12 +140,14 @@ void *serve_single_client(void *arg) {
                         printf("> Blad.\n");
                     }
                     break;
+                // Nowa wiadomosc od obslugiwanego klienta
                 case '2':
                     if(id > -1) {
-                        found = strchr(buf, ';'); //wskaznik na pierwszy srednik w buforze
+                        found = strchr(buf, ';'); // wskaznik na pierwszy srednik w buforze
                         if(found != NULL) {
                             index = (int)(found-buf);
                             strncpy(recvNumber, buf, index);
+                            // Skrocenie zawartosci bufora o numer odbiorcy oraz separator ';'
                             memmove(&buf[0], &buf[index+1], buf_size-index+1);
                             printf("> Nowa wiadomosc od %s do %s: %s.\n", users[id].number, recvNumber, buf);
                         }
@@ -141,6 +157,7 @@ void *serve_single_client(void *arg) {
 
                         recvId = findUser(recvNumber);
                         if(recvId != -1) {
+                            // Dopisanie odebranej wiadomosci do listy messages adresata
                             pthread_mutex_lock(&users[recvId].mutex);
 
                             users[recvId].messageCount++;
@@ -156,7 +173,8 @@ void *serve_single_client(void *arg) {
                             pthread_mutex_unlock(&users[recvId].mutex);
                         }
                         else {
-                            printf("> Nie ma takiego numeru.\n");
+                            printf("Nie ma takiego numeru.\n");
+                            // Przeslanie uzytkownikowi komunikatu o bledzie (nie znaleziono odbiorcy)
                             memset(buf, 0, buf_size);
                             strcpy(buf, "$;Odbiorca nie istnieje.");
                             write(users[id].cfd, &buf, buf_size);
@@ -166,6 +184,7 @@ void *serve_single_client(void *arg) {
                         printf("> Tylko zalogowani moga wysylac wiadomosci.\n");
                     }
                     break;
+                // Wylogowanie klienta
                 case '0':
                     if(id > -1) {
                         users[id].loggedIn = 0;
@@ -194,24 +213,27 @@ void *serve_single_client(void *arg) {
 }
 
 int main() {
+    // Inicjalizacja obiektow koniecznych do realizacji polaczenia z klientem
     struct sockaddr_in addr;
     pthread_t tid;
     int fd = socket(PF_INET, SOCK_STREAM, 0);
     int on=1;
-    setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, (char*)&on, sizeof(on)); //free port number when turned off
+    setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, (char*)&on, sizeof(on));
         addr.sin_family = PF_INET;
         addr.sin_port = htons(1237);
         addr.sin_addr.s_addr = INADDR_ANY;
+    // Przypisanie adresu serwera do gniazda
     bind(fd, (struct sockaddr*) &addr, sizeof(addr));
+    // Maksymalnie 10 oczekujacych polaczen
     listen(fd, 10);
-
+    
     users = malloc(sizeof(User));
     while(1) {
         struct cln* c = malloc(sizeof(struct cln));
         socklen_t len = sizeof(c->caddr);
-        c->cfd = accept(fd, (struct sockaddr*) &c->caddr, &len);
-        pthread_create(&tid, NULL, serve_single_client, c);
-        pthread_detach(tid);
+        c->cfd = accept(fd, (struct sockaddr*) &c->caddr, &len); // zaakceptowanie polaczenia od klienta
+        pthread_create(&tid, NULL, serve_single_client, c); // utworzenie watku obslugujacego klienta
+        pthread_detach(tid); // zasoby wykorzystywane przez watek zostana automatycznie zwolnione po jego zakonczeniu
     }
     close(fd);
     free(users);
